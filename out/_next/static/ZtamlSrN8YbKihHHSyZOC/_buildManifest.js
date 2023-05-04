55677d01b34dffaffe3e5272d19ff0ebcbbe3558
RedFeaTherXll <yuchi9716@gmail.com>
2023-05-04 21:07:45 +0800

    wadwad

--- out/_next/static/ZtamlSrN8YbKihHHSyZOC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/566-2e9a3420273896f4.js","static/chunks/pages/index-2151c02b1d5fe4a6.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
